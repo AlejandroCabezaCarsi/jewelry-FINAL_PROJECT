020e90dd70c10755b025c25240c89939e3479dbc
Alex <cabezacarsialejandro@gmail.com>
2023-07-11 20:30:16 +0200

    [FEAT] Created the migration create_products_table

---
+++ database/migrations/2023_07_11_182649_create_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('material_ID');
+            $table->unsignedBigInteger('type_ID');
+            $table->boolean('diamonds');
+            $table->integer('reference');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
