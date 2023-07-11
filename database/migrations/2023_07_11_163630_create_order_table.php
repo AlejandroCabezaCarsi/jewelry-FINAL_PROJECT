697320bea9c699752e1891bb345669fea0c2c3f0
Alex <cabezacarsialejandro@gmail.com>
2023-07-11 18:42:07 +0200

    [FEAT] Created the migration create_order_table

---
+++ database/migrations/2023_07_11_163630_create_order_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->string('statusOrder');
+            $table->unsignedBigInteger('user_ID');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order');
+    }
+};
